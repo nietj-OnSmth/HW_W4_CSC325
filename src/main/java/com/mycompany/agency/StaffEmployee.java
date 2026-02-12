74577bd7aa8fb78e2a5b434278314200b175fd92
kaleysantonocito <kaleysantonocito31@gmail.com>
2026-02-11 20:24:51 -0500

    //StaffEmployee to do #1 and #2

--- src/main/java/com/mycompany/agency/StaffEmployee.java
+++ src/main/java/com/mycompany/agency/StaffEmployee.java
@@ -3,6 +3,8 @@ package com.mycompany.agency;
 
     // TODO 1: Make StaffEmployee a child of StaffMember
 
+import java.sql.SQLOutput;
+
 public class StaffEmployee extends StaffMember
 {
     protected String socialSecurityNumber;
